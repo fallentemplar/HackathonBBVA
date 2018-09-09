584366649076a9222329ec794a98eaccb244ec60
daniel <daniegarcia10@gmail.com>
2018-09-09 12:57:46 -0500

    Pre

--- app.py
+++ app.py
@@ -19,19 +19,22 @@ class EmpadiController:
     def __init__(self):
         #self.conn = sqlite3.connect("tickets.db")
         self.domain = "ec2-18-191-203-36.us-east-2.compute.amazonaws.com"
-        self.server_url = "http://" + self.domain + ":5000/"
+        self.server_url = "http://" + self.domain #+ ":5000/"
         #self.cur = self.conn.cursor()
         self.apache_path = "/var/www/html/"
         self.f_name = "ticket.pdf"
         self.f_hash_str = None
 
     def create_ticket(self, data):
-        self.__create_pdf(data)
+        if data is not None:
+            self.__create_pdf(data)
         self.__store_file()
         return self.__short_url()
 
     def __create_pdf(self, data: dict):
+        print(data)
         c = canvas.Canvas(self.apache_path + self.f_name, pagesize=letter)
+        c.drawImage("logoBBVA.jpg",10,650, width=200, height=180)
         c.setFont("Helvetica",12)
         c.drawString(185,700,"MIREYA BERENICE RODRIGUEZ BAHENA")
         c.setFillColor(blue)
@@ -44,26 +47,26 @@ class EmpadiController:
         c.setFont("Helvetica",12)
         c.drawString(150,610,"No. de cajero:       D001")
         c.drawString(116,590,"Ubicación de cajero:       PRACT 2100XE CO")
-        c.drawString(137,570,"Cuenta de retiro:       " + data["user"])
-        c.drawString(123,550,"Nombre:       " )
+        c.drawString(137,570,"Cuenta de retiro:       " + data["noCuenta"])
+        c.drawString(123,550,"Tipo operación:       " + data["tipoOperacion"] )
         c.drawString(133,530,"Importe ingresado:       ")
-        c.drawString(118,510,"Importe depositado:       ")
-        c.drawString(125,490,"Motivo de pado:       ")
-        c.drawString(125,490,"Hora de operación:       10:31:40")
-        c.drawString(124,470,"Folio de operación:       7316")
-        c.drawString(86,450,"Autorización de operación:")
+        c.drawString(118,510,"Importe depositado:       " + data["monto"])
+        c.drawString(125,490,"Motivo de pago:       ")
+        c.drawString(125,470,"Fecha de operación:       " + data["fecha"])
+        c.drawString(124,450,"Folio de operación:       " + data["numeroTransaccion"])
+        c.drawString(86,430,"Ciudad:        " + data["ciudadOrigen"])
         c.setFillColor(lightblue)
-        c.rect(26,320,410,120, fill = True, stroke=False)
+        c.rect(26,320,390,120, fill = True, stroke=False)
         c.setFillColor(black)
         c.setFont("Helvetica",13)
-        c.drawString(29,420,"En caso de no reconocer esta operación, comunícate al 01800 22 62")
-        c.drawString(29,400,"663")
-        c.drawString(29,370,"Este documento constituye una notificación de los términos en")
-        c.drawString(29,350,"que se realizo la operación, el unico comprobante oficial es el estado")
-        c.drawString(29,330,"de cuenta que emite BBVA Bancomer.")
+        c.drawString(29,390,"En caso de no reconocer esta operación, comunícate al 01800 22 62")
+        c.drawString(29,370,"663")
+        c.drawString(29,350,"Este documento constituye una notificación de los términos en")
+        c.drawString(29,330,"que se realizo la operación, el unico comprobante oficial es el estado")
+        c.drawString(29,310,"de cuenta que emite BBVA Bancomer.")
         c.save()
 
-    def __send_sms(self):
+    def send_sms(self):
         account_sid = 'AC747e71bccd71f5b22518b9816201a2a3'
         auth_token = '07631880370761a742b614de5a23011d' 
         myPhone = '+524421164682' 
@@ -76,11 +79,11 @@ class EmpadiController:
 
     def __store_file(self):
         with open(self.apache_path + self.f_name, "rb") as f:
-            self.f_hash_str = str(hashlib.sha3_256(f.read()).hexdigest())
-            os.rename(self.f_name,self.apache_path + "consult/" + self.f_hash_str)
+            self.f_hash_str = str(hashlib.sha256(f.read()).hexdigest())
+            os.rename(self.apache_path + self.f_name, self.apache_path + "consult/" + self.f_hash_str + ".pdf")
 
     def __short_url(self):
-        url_target = self.server_url + "consult/" + self.f_hash_str + ".pdf"
+        url_target = self.server_url + "/consult/" + self.f_hash_str + ".pdf"
         shortener_url = "https://api.rebrandly.com/v1/links"
         api_key = "51b9f465980d4ef4b16dcaeab2b46a6d"
         data = json.dumps({"destination": url_target, "domain":
@@ -123,6 +126,11 @@ def create():
         log.debug("url original: {}\nurl corta: {}".format(link["destination"], link["shortUrl"]))"""
     return controller.create_ticket(request.get_json())
 
+@app.route("/sms")
+def send_sms():
+    j = request.get_json()
+    controller.send_sms()
+    return "{}"
 
 @app.route("/consult/<ticketid>")
 def consult(ticketid):
